b110752e9abe5732b8a925bcaacf6a71ec2da7d9
Sreyahsri S <59528224+sreya-01@users.noreply.github.com>
2023-12-11 11:01:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const elements = document.getElementsByTagName("li");
+for(let i=0; i<elements.length(); i++)
+{
+	elements[i].appendChild(document.createTextNode(" development"));	
+}
+	
